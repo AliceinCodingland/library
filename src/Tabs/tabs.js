27fc73e963cf0fe036488d5b0ccf1084d3f6e4d5
Eden Anbinder <ejanbinder@smcm.edu>
2014-03-13 10:52:30 -0400

    Tabs Progress
    Click "ADD" button to add new tab, which is labeled with its index.
    Click "X" next to tab name to remove that tab.

---
+++ src/Tabs/tabs.js
@@ -0,0 +1,9 @@
+	
+	function getFields(t){
+		//Input: Some int t representing the number of the tab that has been selected
+		//Output: The field values or the entire .fields class associated with that tab
+		var fieldInfo = new String("<p>THIS IS TAB" + t + "</p>");
+		return fieldInfo;
+		
+	}
+	
